5f98144e466905fd407cbc7299b6f6b2d9b1d6cb
Kaiki2814 <kaiki.samuel1428@gmail.com>
2021-07-06 17:57:00 -0300

    Trabalhando com atribução variaveis

---
+++ meuprimeroPrograma/5-atribuicaoVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribução variaveis");
+
+const idade = 29;
+const nome = "Kaiki";
+const sobrenome = "Samuel";
+
+//console.log(nome + " " + sobrenome)
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`); 
+
+let contador = 0;
+contador = contador +1
+const nomeCompleto = primeroNome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto =2;
