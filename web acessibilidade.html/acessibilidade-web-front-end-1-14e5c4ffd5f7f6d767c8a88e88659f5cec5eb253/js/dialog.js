0e0d798e4eea85aa37991ad804ffb70351056478
Kaiki2814 <kaiki.samuel1428@gmail.com>
2021-07-16 18:04:02 -0300

    web acessibilidade

--- web acessibilidade.html/acessibilidade-web-front-end-1-14e5c4ffd5f7f6d767c8a88e88659f5cec5eb253/js/dialog.js
+++ web acessibilidade.html/acessibilidade-web-front-end-1-14e5c4ffd5f7f6d767c8a88e88659f5cec5eb253/js/dialog.js
@@ -4,15 +4,18 @@ var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
+bbtnAbreDialog.style.display = 'block';
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus()
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');    
+  conteudoForaDialog.inert = 'false' 
 }
 
 // Listeners
